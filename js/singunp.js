a6d8ab30d62e9b1832288def144d13d28116b2b2
valeriavalles <valeria.sv@outlook.es>
2017-12-29 21:55:47 -0500

    actualizando html singup

--- js/singunp.js
+++ js/singunp.js
@@ -2,16 +2,33 @@
 
 $(document).ready(function() {
    /*habilitando/desabilitando el boton */
-    var area = $('#number');
+    var content = $('#number');
     var expresion = /^[0-9]+$/;
+    var numero = 0;
+     numero = Math.floor(Math.random()*100 + 100);
 
     $('#number').on('keyup',function(){
 
-        if(area.val().length == 10){
-            $('#area').attr('disabled',false);  
+        if(content.val().length == 10){
+            $('#area').attr('disabled',false);
+            
         }else  {
             $('#area').attr('disabled',true); 
         }  
-    })    
+    })
+    /* Generando numeros aleatorios */
+
+   /* $('#area').on('click',function(){
+        alert(numero);
+    }) */
+    var boton = document.getElementById('area');
+    boton.addEventListener('click',numeroGenerado );
+    function numeroGenerado(){
+    var numero = 0;
+    numero = Math.floor(Math.random()*100)+100;
+    var codigo = "Tu código: Lab-"+ numero;
+    alert(codigo);
+    }
+
 });
-    
+
